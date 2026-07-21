05642d5187813ba9933955e573a56aeb77ecbc42
Tarun-tej-2007 <taruntej947@gmail.com>
2026-07-21 22:03:56 +0530

    implement graph serialization

---
+++ analysis-engine/app/graph/serialization/exceptions.py
@@ -0,0 +1,10 @@
+"""Graph serialization exceptions module.
+
+Defines the exception hierarchy for graph serialization and deserialization operations.
+"""
+
+
+class GraphSerializationError(Exception):
+    """Base exception class for graph serialization and deserialization errors."""
+
+    pass
